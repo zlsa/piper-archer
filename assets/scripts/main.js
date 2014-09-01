68f4f1887aa84ba50a5e235f69a8c61159669124
Jon Ross <zlsa@outlook.com>
2014-09-01 11:18:21 -0700

    improvements to scripts and speed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -12,28 +12,37 @@ function tab(t) {
 }
 
 $(document).ready(function() {
-  $.get("https://api.github.com/repos/zlsa/"+repo+"/commits/master", function(data) {
-    if(data.length >= 1) {
-      var c = data[0];
-    } else {
-      var c = data;
-    }
-    var commit = c.commit.message;
-    var time   = new Date(Date.parse(c.commit.committer.date));
-    $("#last-commit .message").text(commit);
-    $("#last-commit").removeClass("fetching");
-    var s      = "";
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    s         += time.getDate() + " ";
-    s         += months[time.getMonth()] + " ";
-    s         += time.getFullYear();
-    $(".last-update .date").text(s);
-  });
+  if(false) {
+    $.get("https://api.github.com/repos/zlsa/"+repo+"/commits/master", function(data) {
+      if(data.length >= 1) {
+        var c = data[0];
+      } else {
+        var c = data;
+      }
+      var commit = c.commit.message;
+      var time   = new Date(Date.parse(c.commit.committer.date));
+      $("#last-commit .message").text(commit);
+      $("#last-commit").removeClass("fetching");
+      var s      = "";
+      var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      s         += time.getDate() + " ";
+      s         += months[time.getMonth()] + " ";
+      s         += time.getFullYear();
+      $(".last-update .date").text(s);
+    });
+  }
   for(var i=info.releases.length-1;i>=0;i--) {
     var release = info.releases[i];
     if(release[0] == "pa28-181-latest.zip") {
+      var s      = "";
+      var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      s         += release[1].substr(6, 2) + " ";
+      s         += months[parseInt(release[1].substr(4, 2)) - 1] + " ";
+      s         += release[1].substr(0, 4);
+      $(".last-update .date").text(s);
       $(".size").text(release[2] + " MB");
+    } else {
+      $("#tab-previous").append("<div class='release'><a class='download' href='http://zlsa.github.io/pa28-181/releases/"+release[0]+"' title='Download this older release'>"+release[0]+"</a><span class='size'>"+release[2]+" MB</span></div>");
     }
-    $("#tab-previous").append("<div class='release'><a class='download' href='http://zlsa.github.io/pa28-181/releases/"+release[0]+"' title='Download this older release'>"+release[0]+"</a><span class='size'>"+release[2]+" MB</span></div>");
   }
 });
--- collect.py
+++ collect.py
@@ -23,7 +23,7 @@ def collect(path_from = "../releases/", path_to = "releases/"):
       shutil.copy(os.path.join(path_from, filename), os.path.join(path_to, filename))
       date = int(os.path.splitext(filename)[0][len(AIRCRAFT)+1:])
       size = round(os.path.getsize(os.path.join(path_to, filename)) / 1024 / 1024, 2)
-      files.append((filename, date, size))
+      files.append((filename, str(date), size))
       if date > latest:
         latest_filename = filename
         latest_size = size
@@ -34,7 +34,7 @@ def collect(path_from = "../releases/", path_to = "releases/"):
   if latest:
     shutil.copy(os.path.join(path_to, latest_filename), os.path.join(path_to, AIRCRAFT + "-latest.zip"))
     print(AIRCRAFT+"-latest.zip  " + (str(size) + " MB").rjust(10, " "))
-    files.append((AIRCRAFT+"-latest.zip", latest_date, latest_size))
+    files.append((AIRCRAFT+"-latest.zip", str(latest_date), latest_size))
   else:
     print("no aircraft copied")
   info = {}
