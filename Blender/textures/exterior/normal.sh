66452fd62e44c4c9f8520869b6d73eb0f916a5f2
Jon Ross <zlsa@outlook.com>
2014-09-10 16:48:31 -0700

    normal map toolchain

---
+++ Blender/textures/exterior/normal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# converts bump map (bump.png) to normal map (normal.png). You will need a svg2png script that converts svg to png (duh).
+
+TMP=normal-tmp
+
+mkdir $TMP
+svg2png bump.svg
+#convert bump.png +level 30%,60% -adaptive-blur 3 bump.png
+convert bump.png +level 30%,60% bump.png
+nvcompress -fast -tonormal bump.png $TMP/normal.dds
+nvdecompress $TMP/normal.dds
+convert $TMP/normal.tga normal.png
+rm $TMP -rf
